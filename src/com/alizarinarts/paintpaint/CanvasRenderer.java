fc6ad97902f39da3847f13de0b0f0983f0f2a847
Rose E. Shere <rose.e.shere@gmail.com>
2017-10-22 20:00:22 -0700

    Update CanvasRenderer.java

--- src/com/alizarinarts/paintpaint/CanvasRenderer.java
+++ src/com/alizarinarts/paintpaint/CanvasRenderer.java
@@ -37,7 +37,7 @@ import android.util.Log;
  * then drawn to an invisible framebuffer texture.  This texture is then drawn
  * to a polygon on the screen framebuffer for display.
  *
- * @author <a href="mailto:david.e.shere@gmail.com">David Shere</a>
+ * @author <a href="mailto:rose.e.shere@gmail.com">Rose Shere</a>
  */
 public class CanvasRenderer implements GLSurfaceView.Renderer {
 
